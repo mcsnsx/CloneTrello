c52759c6f9bed312d167488b9f4642dec97b5cd6
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2022-06-09 17:31:14 -0300

    Inicio CloneTrello

---
+++ kanbanboard/src/test/java/org/seariver/kanbanboard/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seariver.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
