3cd05d80dd4a635792c0f9af9a8b985e82139746
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2022-06-09 18:37:33 -0300

    Clone Trello

--- kanbanboard/src/test/java/org/seariver/kanbanboard/KanbanBoardApplicationTests.java
+++ kanbanboard/src/test/java/org/seariver/kanbanboard/KanbanBoardApplicationTests.java
@@ -2,12 +2,14 @@ package org.seariver.kanbanboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class KanbanBoardApplicationTests {
+public class KanbanBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
 
 }
